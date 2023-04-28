19788d0209e161331b195b0045e089bb68c06260
rjnpgarcia <102405417+rjnpgarcia@users.noreply.github.com>
2023-04-29 03:17:03 +0800

    Update clear button TTS

--- client/src/components/TextToSpeech.js
+++ client/src/components/TextToSpeech.js
@@ -239,6 +239,7 @@ const TextToSpeech = () => {
     if (fileInputTTSRef.current) {
       fileInputTTSRef.current.value = "";
     }
+    setTextData([]);
     setFileData({});
     setOverwriteFile({});
     setShowDeleteModal(false);
